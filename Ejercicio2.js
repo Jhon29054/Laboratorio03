ed0f8735d13b5c99ee90622e51171aa9d2a89a58
Jhon29054 <146318204+Jhon29054@users.noreply.github.com>
2023-12-19 11:46:55 -0500

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,22 @@
+var bandera = false;
+console.log(typeof bandera);
+function miFuncion(){}
+console.log(typeof miFuncion);
+var Simbolo =Symbol("mi símbolo");
+console.log(typeof Simbolo);
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona);
+
+var X;
+console.log(typeof x);
+
+x= undefined;
+console.log ( typeof x );
+
+var y = null;
+console.log(typeof y);
